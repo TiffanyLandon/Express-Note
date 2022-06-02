01a62275cb2d48c61325e916ea2a28f1d65ab8a6
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-06-02 05:14:35 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { findById } = require('../../../Downloads/11.4(1)/lib/animals');
-const { title } = require('process');
+const uuid = require('./helpers/uuid');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,25 +14,35 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-  res.readFile('./Develop/db/db.json', "utf8", (err, jsonString))
+  fs.readFile("./db/db.json", "utf8", (err, jsonString) => {
+    currentNotes = jsonString ? JSON.parse(jsonString) : [];
+    res.json(currentNotes);
+  })
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  const note = findById(req.params.id, notes)
-  if (note) {
-    res.json(note);
-  } else {
-    res.send(404);
+app.get('/api/notes:note_id', (req, res) => {
+  if (req.body && req.params.review_id) {
+    console.info(`${req.method} request received to get a single a note`);
+    const noteId = req.params.note_id;
+    for (let i = 0; i < note.length; i++) {
+      const currentNote = notes[i];
+      if (currentNotes.notes_id === notesId) {
+        res.json(currentNote);
+        return;
+      }
+    }
+    res.json('Note ID not found');
   }
 });
+
 
 app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
@@ -42,34 +52,35 @@ app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
 
   // If all the required properties are present
-  if (id && title && username) {
+  if (title && text) {
     // Variable for the object we will save
     const newNote = {
-      id,
       title,
-      username,
+      text,
       note_id: uuid(),
     };
 
-    fs.readFile("./db/db.json", "utf8", (err, jsonString) => {
-      // currentReviews = jsonString ? JSON.parse(jsonString) : [];
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
 
-      if (jsonString) {
-        currentNotes = JSON.parse(jsonString);
+      if (err) {
+        console.error(err);
       } else {
-        currentNotes = [];
-      }
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
 
-      currentNotes = [...currentNotes, newNote];
+        // Add a new review
+        parsedNotes.push(newNote);
 
-      noteString = JSON.stringify(currentNotes);
-
-      //Write the string to a file
-      fs.writeFile(`./db/db.json`, noteString, (err) =>
-        err
-          ? console.error(err)
-          : console.log(`${newNote.title} has been written to JSON file`)
-      );
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+      };
     });
 
     const response = {
@@ -84,31 +95,56 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-app.delete("/notes/:id", (req, res) => {
-  const deleteId = req.params.id;
-  var newArray = [];
-
+app.delete("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const db = JSON.parse(data);
-      newArray = notes.filter((note) => {
-        if (note.id !== deleteId) {
-          return true;
+    console.info(`${req.method} request received to delete a note`);
+
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will delete
+      const deleteNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+
+      fs.readFile("./db/db.json", "utf8", (err, data) => {
+
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+
+          // Add a new review
+          parsedNotes.unlink(deleteNote);
+
+          // Write updated reviews back to the file
+          fs.deleteFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully delete note!')
+          );
         }
       });
-      console.log(newArray)
-      fs.writeFile(
-        path.join(__dirname, "../../db/db.json"),
-        JSON.stringify(newArray, null, 2),
-        (err) => {
-          if (err) throw err;
-          else res.status(200);
-        }
-      );
+
+      const response = {
+        status: "success",
+        body: deleteNote,
+      };
+
+      console.log(response);
+      res.json(response);
+    } else {
+      res.json("Error in posting note");
     }
-  });
+  })
 });
 
 
